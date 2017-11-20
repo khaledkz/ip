982d8ebb8738077a23d1439447ac48ebecdf3713
kharazzoro <kha.1991.14.2017@gmail.com>
2017-11-20 14:03:17 +0000

    up

--- server.js
+++ server.js
@@ -8,34 +8,34 @@ app.use(express.static("public", { 'extensions': ['html'] }));
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var xhr = new XMLHttpRequest();
-var myIp = "";
-(
-  function (loading, success) {
-    var xhr = XMLHttpRequest !== undefined ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-    loading.apply(null, []);
-    xhr.open('get', 'https://api.ipify.org/?format=json', true);
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4) {
-        success.call(null, JSON.parse(xhr.responseText));
-      }
-    }
-    xhr.send();
-  }(function () {
-  }, function (response) {
-    myIp = response.ip;
-    console.log(myIp)
+// var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+// var xhr = new XMLHttpRequest();
+// var myIp = "";
+// (
+//   function (loading, success) {
+//     var xhr = XMLHttpRequest !== undefined ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+//     loading.apply(null, []);
+//     xhr.open('get', 'https://api.ipify.org/?format=json', true);
+//     xhr.onreadystatechange = function () {
+//       if (xhr.readyState === 4) {
+//         success.call(null, JSON.parse(xhr.responseText));
+//       }
+//     }
+//     xhr.send();
+//   }(function () {
+//   }, function (response) {
+//     myIp = response.ip;
+//     // console.log(myIp)
 
-var nodemailer = require('nodemailer');
+// var nodemailer = require('nodemailer');
 
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'wafa.1998.13.2018@gmail.com',
-    pass: 'wafa2015'
-  }
-});
+// var transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: 'wafa.1998.13.2018@gmail.com',
+//     pass: 'wafa2015'
+//   }
+// });
 
 // let ipAddress="My Ip:"+myIp;
 //  var mailOptions = {
@@ -54,13 +54,26 @@ var transporter = nodemailer.createTransport({
 // });
 
 
-  }));
+  // }));
 
 
-
+  var os = require('os');
+  
+  var interfaces = os.networkInterfaces();
+  var addresses = [];
+  for (var k in interfaces) {
+      for (var k2 in interfaces[k]) {
+          var address = interfaces[k][k2];
+          if (address.family === 'IPv4' && !address.internal) {
+              addresses.push(address.address);
+          }
+      }
+  }
+  
+  console.log(addresses);
 
 app.get('/', function (req, res) {
-  res.render('index', { myIp: myIp });
+  res.render('index', { myIp: addresses });
 });
 
 
